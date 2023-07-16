e781749a378291fa0a6375fedd899931c46a09cf
Akshat Sharma <78557805+akshit-2002@users.noreply.github.com>
2023-07-17 00:45:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import useClipboard from "react-use-clipboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    const refresh = () => window.location.reload(true)
+    const [textToCopy,setTextToCopy]=useState();
+    const [isCopied, setCopied] = useClipboard(textToCopy);
+    const startListening = () => SpeechRecognition.startListening({ continuous: true, language : 'en-IN' })
+    const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+    if (!browserSupportsSpeechRecognition) {
+        return null
+      } 
+      
+     
+    return (
+        <>
+            <div className="container">
+                <h2> SPEXTER </h2>
+                <br/>
+                <p>A Site that converts speech from the microphone to text and makes it available to your use. </p>
+                <p className="para2">Just click in the box before procced to copy the text.</p>
+
+                <div className="main-content" onClick={ () => setTextToCopy(transcript)}>
+                    {transcript}
+                </div>
+                <div className="same-btn-style">
+                <div className="btn-style">
+                    
+                    <button onClick={startListening}>Start Listening </button>
+                    <button onClick={SpeechRecognition.stopListening}>Stop Listening </button><br />
+                    
+                </div>
+                  <div className="btn-style">
+                    <button onClick={setCopied}> {isCopied ? "Yes! Copied" : 'Copy To ClipBoard'}  </button>
+                    <button onClick={refresh}>Refresh</button>
+                  </div>
+                </div>
+            </div>
+        </>
+    );
+};
 
 export default App;
